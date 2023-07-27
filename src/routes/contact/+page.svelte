2bfbc12a154f843a0c73a83de10f29ec1b799bc5
Alexander Damov <alexsander.damov@yahoo.com>
2023-07-27 21:19:40 +0300

    Resume page - In progress. Project page - Done. Home page - Done.

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Header from '../+page.svelte';
+</script>
+
+<Header/>
+<h1>Contact</h1>
+
+<style>
+    h1 {
+        color: white;
+    }
+</style>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import Header from "../+page.svelte"
+</script>
+
+<Header/>
+<h1>My projects</h1>
+<ul>
+    <li>
+        <a href="https://github.com/st0ppers/ExpenseAPI">Expense API</a>
+        <p>Simple CRUD Api that works with mongoDB to store my expenses for each month.</p>
+    </li>
+    <li>
+        <a href="https://github.com/st0ppers/ExpenseWithReact">Expense frontend</a>
+        <p>The front end that visualizes the records in the DB.</p>
+    </li>
+    <li>
+        <a href="https://github.com/st0ppers/DraftKingsAcademyProject">Academy project</a>
+        <p>Academy project for DraftKings</p>
+    </li>
+</ul>
+<style>
+    h1 {
+        color: white;
+    }
+
+    ul {
+        color: white;
+        list-style: none;
+    }
+
+    li {
+        color: white;
+        padding: 10px;
+    }
+
+    a {
+        color: white;
+    }
+
+    p {
+        color: white;
+        display: flex;
+    }
+</style>
