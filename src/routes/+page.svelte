a1d745b3490cf72534950f4699bb82d253068ee5
Alexander Damov <alexsander.damov@yahoo.com>
2023-08-06 20:58:06 +0300

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Test</p>
+<div class="wrapper">
+    <a href="/resume">Resume</a>
+    <a href="/home">About me</a>
+    <a href="/contact">Contact</a>
+</div>
+
+
+
+<style>
+    .wrapper{
+        display: flex;
+        justify-content: end;
+        padding: 20px;
+    }
+    a{
+        text-decoration: none;
+        padding-left: 20px;
+        color: white;
+    }
+
+</style>
