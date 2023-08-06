9d08c2efed291ee2cb865723fa6d3074ec317846
Alexander Damov <alexsander.damov@yahoo.com>
2023-08-06 20:59:49 +0300

    asdf
    asdf

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+    import Header from "../+page.svelte";
+</script>
+
+<Header/>
+<div class="wrapper">
+    <img src="https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3=w240-h480-rw"
+         alt="smily">
+    <div>
+        <p>Hello,</p>
+        <p>I am <b>Alexander Damov</b>, junior full stack developer currently working in DraftKings</p>
+        <p>Also I am pursuing a bachelors degree in Software technology.</p>
+        <a href="/projects">My projects</a>
+    </div>
+</div>
+
+
+<style>
+    .wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        justify-items: center;
+        flex-wrap: wrap;
+        width: 100vh;
+        margin: auto;
+        height: 100vh;
+        overflow: hidden;
+        background-color: #222222;
+    }
+
+    img {
+        width: 20%;
+        height: 20%;
+        padding: 30px;
+        border-radius: 50%;
+    }
+
+    p {
+        font-size: 20px;
+        color:white;
+    }
+
+    a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-decoration: none;
+        color:white;
+        width: 100%;
+        font-size: 20px;
+    }
+</style>
