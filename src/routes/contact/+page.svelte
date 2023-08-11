bdef3f1bd7b6c2b22843f8c26c23b3c69f6b435b
Alexander Damov <alexsander.damov@yahoo.com>
2023-08-11 22:59:01 +0300

    Only footer left

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,5 +1,69 @@
 <script lang="ts">
     import Navigation from "../+page.svelte";
+    import Heading from "../shared/Heading.svelte";
+    import Footer from "../shared/Footer.svelte";
+    import Cellphone from "svelte-material-icons/Cellphone.svelte";
+    import Mail from "svelte-material-icons/Email.svelte";
 </script>
+<div>
+    <Navigation/>
+    <Heading content="Contact"/>
+    <div class="wrapper">
+        <div class="img">
+            <div class="picture">
+                <Cellphone color="white" size="5rem"/>
+            </div>
+            <div class="text-content">
+                <p>Call me</p>
+            </div>
+            <div class="text-content">
+                <p>089-241-0428</p>
+            </div>
+        </div>
+        <div class="img">
+            <div class="picture">
+                <Mail color="white" size="5rem"/>
+            </div>
+            <div class="text-content">
+                <p>Email me</p>
+            </div>
+            <div class="text-content">
+                <p>alexsander.damov@yahoo.com</p>
+            </div>
+        </div>
+    </div>
+    <Footer/>
+</div>
 
-<Navigation/>
+<style>
+
+    .wrapper {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        height: 69%;
+        margin-top: 5%;
+    }
+
+    .img {
+        width: 50%;
+        margin-left: 10%;
+    }
+
+    .picture {
+        text-align: center;
+    }
+
+    .text-content {
+        text-align: center;
+        color: white;
+        font-weight: bold;
+        font-size: 1.5rem;
+        font-family: Roboto, monospace
+    }
+
+    .view-height {
+        height: 100vh;
+    }
+</style>
---
+++ src/routes/shared/Footer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Instagram from "svelte-material-icons/Instagram.svelte";
+    import GitHub from "svelte-material-icons/Github.svelte";
+    import LinkedIn from "svelte-material-icons/Linkedin.svelte";
+</script>
+<div class="wrapper">
+    <Instagram color="white" size="2rem"/>
+    <GitHub color="white" size="2rem"/>
+    <LinkedIn color="white" size="2rem"/>
+</div>
+
+<style>
+    .wrapper {
+        display: flex;
+        justify-content: center;
+        margin-top: auto;
+    }
+</style>
